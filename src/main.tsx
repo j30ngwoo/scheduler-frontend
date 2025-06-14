4e340aeb5f21426c49fd0bb565a7c7ef0b086b0d
j30ngwoo <ttangle1234@naver.com>
2025-06-14 20:50:39 +0900

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <div style={{ color: "red", fontSize: 40 }}>HELLO WORLD!</div>
+  </React.StrictMode>
+);
